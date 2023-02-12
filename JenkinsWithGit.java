a5ebacb5f3dcbcd9ef8bc37bda4e7a727fad6dcc
Ali Basodan <a_basodan@hotmail.com>
2023-02-12 19:02:14 +0300

    Modified JenkinsWithGit.java

--- JenkinsWithGit.java
+++ JenkinsWithGit.java
@@ -1,15 +1,10 @@
-public miscelleneous;
+package miscelleneous;
 
 public class JenkinsWithGit {
 	public static void main(String[] args) {
-		int charPerLine = 0;
-
-		for (int i = 33; i < 256; i++, charPerLine++) {
-			
-			System.out.print((char)i+((charPerLine%10==0)?"\n":"\t"));
-			System.out.print();
-			
-			}
-		}
+		
+		System.out.println("make it simple :)");
 
 	}
+
+}
