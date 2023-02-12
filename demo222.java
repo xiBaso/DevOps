6a3125a75f22a26c891e5490f10313470cd51270
Ali Basodan <a_basodan@hotmail.com>
2023-02-12 11:17:26 +0300

    idk

--- demo222.java
+++ demo222.java
@@ -1,8 +1,7 @@
 public class DEMO {
 
 	public static void Main(String[] args) {
-		
-		for(int i=0; i<10; i++)
-			System.out.println("Welcome to Java");
+
+		System.out.println("Welcome to Java");
 	}
 }
